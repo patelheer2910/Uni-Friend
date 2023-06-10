4ecfb9ad96a67d4a5012a2fc684d92b3fc90a2ee
VIDHI BHATT <83345228+VidhiBhatt01@users.noreply.github.com>
2023-06-10 11:54:48 +0530

    Delete Uni - Friend directory

--- Uni - Friend/JS/index.js
+++
@@ -1,7 +0,0 @@
-/*this is an event listener for toggle button*/
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
